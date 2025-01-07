ffc54e6b6036be48ca9c19792fa36b82d8c21985
Guilherme Lino <guilhermelinosp@gmail.com>
2025-01-07 06:22:41 -0300

    ref: alter all project structure, simplify services

---
+++ product-service/Program.cs
@@ -0,0 +1,30 @@
+using Serilog;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+var host = builder.Host;
+
+host.UseSerilog((context, options) =>
+    options.ReadFrom.Configuration(context.Configuration));
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "product-service");
+    });
+}
+
+app.UseHealthChecks("/health");
+app.UseSerilogRequestLogging();
+
+await app.RunAsync();
